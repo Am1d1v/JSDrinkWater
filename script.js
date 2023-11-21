aef08cb92202abeea7d3cab99e43bb0308a2cbe4
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-11-21 17:03:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 
+
+
+// All small cups
+const smallCups = document.querySelectorAll('.cup-small');
+
+const liters = document.querySelector('#liters');
+const percentage = document.querySelector('#percentage');
+const remained = document.querySelector('#remained');
+
+smallCups.forEach((cup, index) => {
+
+    cup.addEventListener('click', () => highlighCups(index));
+});
+
+
+function highlighCups(cupIndex){
+
+    if(smallCups[cupIndex].classList.contains('full') && !smallCups[cupIndex].nextElementSibling.classList.contains('full')){
+        cupIndex--;
+    }
+
+    smallCups.forEach((cup, index2) => {
+        if(index2 <= cupIndex){
+            cup.classList.add('full');
+        } else {
+            cup.classList.remove('full');
+        }
+    })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
